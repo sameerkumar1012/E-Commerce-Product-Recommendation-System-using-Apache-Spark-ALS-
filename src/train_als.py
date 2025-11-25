c2b09ec7db6908299ae46e7f6bf81e86d6c6d673
Sameer Kumar <142688169+sameerkumar1012@users.noreply.github.com>
2025-11-25 14:55:35 +0530

    Update train_als.py

--- src/train_als.py
+++ src/train_als.py
@@ -1,2 +1,14 @@
-# train_als.py
-# placeholder script
+from pyspark.ml.recommendation import ALS
+
+def train_als(train_df):
+    als = ALS(
+        maxIter=5,
+        rank=10,
+        regParam=0.1,
+        userCol="userId",
+        itemCol="itemId",
+        ratingCol="rating",
+        coldStartStrategy="drop"
+    )
+    model = als.fit(train_df)
+    return model
