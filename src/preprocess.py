b1c5374e6c27bb4782eae38e604dabd5d75f7d99
Sameer Kumar <142688169+sameerkumar1012@users.noreply.github.com>
2025-11-25 14:55:10 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,2 +1,21 @@
-# preprocess.py
-# placeholder script
+from pyspark.sql.functions import col, when
+
+def preprocess(df, limit_rows=200000):
+    df = df.limit(limit_rows)
+    ratings = df.withColumn(
+        "rating",
+        when(col("event") == "view", 1)
+        .when(col("event") == "addtocart", 2)
+        .when(col("event") == "transaction", 3)
+    )
+    ratings = ratings.select(
+        col("visitorid").alias("userId"),
+        col("itemid").alias("itemId"),
+        col("rating")
+    ).dropna()
+
+    ratings = ratings.withColumn("userId", col("userId").cast("int")) \
+                     .withColumn("itemId", col("itemId").cast("int")) \
+                     .withColumn("rating", col("rating").cast("int"))
+
+    return ratings
