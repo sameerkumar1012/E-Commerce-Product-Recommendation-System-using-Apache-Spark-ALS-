90a086ebe882529af0d3d10cd87b1bf06d5e8e8d
Sameer Kumar <142688169+sameerkumar1012@users.noreply.github.com>
2025-11-25 14:55:51 +0530

    Update recommend.py

--- src/recommend.py
+++ src/recommend.py
@@ -1,2 +1,13 @@
-# recommend.py
-# placeholder script
+from pyspark.sql.functions import explode, col
+
+def user_recommend(model, n=5):
+    userRecs = model.recommendForAllUsers(n)
+    user_recs_exploded = userRecs.select(
+        col("userId"),
+        explode(col("recommendations")).alias("rec")
+    ).select(
+        "userId",
+        col("rec.itemId").alias("itemId"),
+        col("rec.rating").alias("pred_rating")
+    )
+    return user_recs_exploded
